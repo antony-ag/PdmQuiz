1ada6c274d45b64c4c049f5c7564a35fc6e13dac
Adilson Barbosa <adilsonbarbosa.it@gmail.com>
2016-10-05 16:09:54 -0300

    Implementação da tela de teste do quiz

--- app/src/main/java/br/com/fatecpg/pdmquiz/TestActivity.java
+++ app/src/main/java/br/com/fatecpg/pdmquiz/TestActivity.java
@@ -1,7 +1,9 @@
 package br.com.fatecpg.pdmquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -14,4 +16,7 @@ public class TestActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher);
     }
+    public void cancelTest(View View){
+        Intent i =  new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(i);
 }
