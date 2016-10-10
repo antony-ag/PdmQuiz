ed440b8e754b502ef3cfe93a8670379a70bb28ca
Adilson Barbosa <adilsonbarbosa.it@gmail.com>
2016-10-09 23:05:57 -0300

    Validação do EditText e ajustes no layout

--- app/src/main/java/br/com/fatecpg/pdmquiz/MainActivity.java
+++ app/src/main/java/br/com/fatecpg/pdmquiz/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +17,29 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher);
     }
-    public void initTest(View View){
-        Intent i =  new Intent(getApplicationContext(),TestActivity.class);
+
+    public void initTest(View View) {
+        EditText e = getEditText(R.id.editQtd);
+        if (e.getText().toString().length() <= 0) {
+            e.setError("Preencha o campo Quantidade de questões.");
+            e.requestFocus();
+        } else if (Integer.parseInt(e.getText().toString()) < 10 || Integer.parseInt(e.getText().toString()) > 30) {
+            e.setError("Preencha no minimo 10 e no máximo 30");
+            e.requestFocus();
+        } else {
+            Intent i = new Intent(getApplicationContext(), TestActivity.class);
+            i.putExtra("qtdQuestoes", e.getText().toString());
+            startActivity(i);
+        }
+    }
+
+    public void listar(View View) {
+        Intent i = new Intent(getApplicationContext(), HistoricActivity.class);
         startActivity(i);
     }
-    public void listar(View View){
-        Intent i =  new Intent(getApplicationContext(),HistoricActivity.class);
-        startActivity(i);
+
+    public EditText getEditText(int id) {
+        EditText e = (EditText) findViewById(id);
+        return e;
     }
 }
